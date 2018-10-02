81108ce6861902085853b440310efd6342ac6231
Roylves <blueraid1991@gmail.com>
2018-10-02 12:25:43 +0800

    Members-added

---
+++ js/main-added.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".hovermember").hover(function(){
+      $(this).find(".memberpic").fadeIn();
+    }
+                    ,function(){
+                        $(this).find(".memberpic").fadeOut();
+                    }
+                   );        
+});
