3dce26ac36468da384e8b4d0dc43d64b64bc26eb
fazriiii <ahmdfzri@gmail.com>
2018-10-03 19:57:58 +0800

    update

--- js/main-added.js
+++ js/main-added.js
@@ -5,5 +5,10 @@ $(function(){
                     ,function(){
                         $(this).find(".memberpic").fadeOut();
                     }
-                   );        
+                   ); 
+
+    var flkty = new Flickity('.wrap-slick1');
+	var video = document.querySelector('video');
+	video.play();
+         
 });
