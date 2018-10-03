26cd7de7a546ee1bbf8603e7f40e3a05550204de
fazriiii <ahmdfzri@gmail.com>
2018-10-03 20:24:39 +0800

    Revert "update" latest
    This reverts commit 3dce26ac36468da384e8b4d0dc43d64b64bc26eb.

--- js/main-added.js
+++ js/main-added.js
@@ -5,10 +5,5 @@ $(function(){
                     ,function(){
                         $(this).find(".memberpic").fadeOut();
                     }
-                   ); 
-
-    var flkty = new Flickity('.wrap-slick1');
-	var video = document.querySelector('video');
-	video.play();
-         
+                   );        
 });
